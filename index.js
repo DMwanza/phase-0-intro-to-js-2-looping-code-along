42b856da9d0654a13dd77beaf0b9ae4107d630f6
Dennis Musembi <dennismusembi344@gmail.com>
2023-03-12 03:51:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+const names=["Guadalupe", "Ollie", "Aki"];
+function writeCards(names) {
+    let messages = [];
+    names.forEach((name, index) => {
+      messages.push("Thank you, " + name + ", for the wonderful surprise gift!");
+    });
+    return messages;
+  }
+  
+  console.log(writeCards(names));
+function countDown(){
+for (let countDown=10;countDown >= 0;countDown--)
+ {console.log(countDown)
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
