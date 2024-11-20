e8614ffd2bdc45133a858d8b839632b3949073c4
Dennis <dennismusembi344@gmail.com>
2024-11-20 14:22:46 +0300

    reDone

--- index.js
+++ index.js
@@ -14,24 +14,40 @@
   // }
   // }
   
-  const names=["Guadalupe", "Ollie", "Aki"];
-  function writeCards(names,event){
-    let newArray=[]
-    for(let i=0;i<names.length;i++){
-      let message=`Thank you, ${names[i]}, for the wonderful ${event} gift!`
-      newArray.push(message)
+  // const names=["Guadalupe", "Ollie", "Aki"];
+  // function writeCards(names,event){
+  //   let newArray=[]
+  //   for(let i=0;i<names.length;i++){
+  //     let message=`Thank you, ${names[i]}, for the wonderful ${event} gift!`
+  //     newArray.push(message)
+  //   }
+  //   return newArray
+  // }
+  // function countDown(num){
+  //   let i=num
+  //   while(i>=0){
+  //     console.log(i--)
+  //   }
+  //   return num
+  // }
+  // countDown(10)
+
+const names=["Guadalupe", "Ollie", "Aki"];
+
+const writeCards=function(names){
+  let newArray=[]
+  for(let i=0;i<names.length;i++){
+      let messages=(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+      newArray.push(messages)
     }
     return newArray
+}
+function countDown(num){
+  for(let i=num;i>=0;i--){
+    console.log(i)
   }
-  function countDown(num){
-    let i=num
-    while(i>=0){
-      console.log(i--)
-    }
-    return num
-  }
-  countDown(10)
-
+  return num
+}
 
 
 
